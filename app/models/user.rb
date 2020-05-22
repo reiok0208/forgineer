47dae3dec3e674158883fe048da3018f56831186
reiok0208 <rei.okada.wii@gmail.com>
2020-05-22 20:44:11 +0900

    [修正]ナビバーをドロワーメニューに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,28 @@ class User < ApplicationRecord
   has_many :diaries, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
-  has_many :followings, through: :active_relationships, source: :follower
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
-  has_many :followers, through: :passive_relationships, source: :following
+  has_many :following, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy # フォロー取得
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :following, source: :follower # 自分がフォローしている人
+  has_many :follower_user, through: :follower, source: :following # 自分をフォローしている人
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(follower_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(following_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 
   validates :name, presence: true
   validates :nickname, presence: true
-
-  def followed_by?(user)
-    passive_relationships.find_by(following_id: user.id).present?
-  end
-
 
   attachment :profile_image
 end
