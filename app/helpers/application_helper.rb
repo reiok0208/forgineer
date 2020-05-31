f2e23629b2d5a2e7d748bbdccf0cd7b4403e5387
reiok0208 <rei.okada.wii@gmail.com>
2020-05-31 14:31:56 +0900

    [追加]フッター追加、細かな修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,7 +47,6 @@ module ApplicationHelper
       html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
       options = {
           autolink: true,
-          space_after_headers: true,
           no_intra_emphasis: true,
           fenced_code_blocks: true,
           tables: true,
