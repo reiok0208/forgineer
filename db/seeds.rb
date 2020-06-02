1f6449756345f0c8db9aae61d3bdbba5c8a192b2
reiok0208 <rei.okada.wii@gmail.com>
2020-06-02 13:51:06 +0900

    [修正]seedを本番用に修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,19 @@
 
 User.create!([
   #user_id1は管理者、user_id2は非ログインのユーザーのコメントに使用
-  {name: "管理者",nickname: "かんりしゃ",email: "admin@mail.com",password: "admin1111",password_confirmation: "admin1111",admin: true},
-  {name: "非会員",nickname: "ひかいいん",email: "test1@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test2@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test3@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test4@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test5@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test6@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test7@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test8@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test9@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test10@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test11@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test12@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test13@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test14@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
-  {name: "無名",nickname: "むめい",email: "test15@mail.com",password: "test1111",password_confirmation: "test1111",admin: false}
+  {name: "管理者",nickname: "管理者",email: "admin@mail.com",password: "admin1111",password_confirmation: "admin1111",admin: true},
+  {name: "非会員",nickname: "非会員",email: "test1@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "テスト会員",nickname: "テスト",email: "test1@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "町田悠里",nickname: "まっちー",email: "test2@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "西川真人",nickname: "にっしー",email: "test3@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "関根宏侑",nickname: "せっきー",email: "test4@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "原荘介",nickname: "はらっち",email: "test5@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "原口長生",nickname: "ぐっちー",email: "test6@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "橋本良秋",nickname: "はっしー",email: "test7@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "平川征輝",nickname: "ひら",email: "test8@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "吉岡好利",nickname: "よっしー",email: "test9@mail.com",password: "test1111",password_confirmation: "test1111",admin: false},
+  {name: "新谷酉蔵",nickname: "あらやん",email: "test10@mail.com",password: "test1111",password_confirmation: "test1111",admin: false}
 ])
-
-20.times do
-  Diary.create!(
-    user_id: 3,
-    title: "タイトル",
-    body: "内容です"
-  )
-end
 
 Tag.create!([
   {name: "rubyonrails"},
@@ -47,7 +35,6 @@ Tag.create!([
 ])
 
 Relationship.create!([
-  {follower_id: 3,followed_id: 16},
   {follower_id: 3,followed_id: 4},
   {follower_id: 3,followed_id: 5},
   {follower_id: 3,followed_id: 6},
@@ -57,9 +44,6 @@ Relationship.create!([
   {follower_id: 3,followed_id: 10},
   {follower_id: 3,followed_id: 11},
   {follower_id: 3,followed_id: 12},
-  {follower_id: 3,followed_id: 13},
-  {follower_id: 3,followed_id: 14},
-  {follower_id: 3,followed_id: 15},
   {follower_id: 4,followed_id: 3},
   {follower_id: 5,followed_id: 3},
   {follower_id: 6,followed_id: 3},
@@ -69,8 +53,4 @@ Relationship.create!([
   {follower_id: 10,followed_id: 3},
   {follower_id: 11,followed_id: 3},
   {follower_id: 12,followed_id: 3},
-  {follower_id: 13,followed_id: 3},
-  {follower_id: 14,followed_id: 3},
-  {follower_id: 15,followed_id: 3},
-  {follower_id: 16,followed_id: 3}
 ])
