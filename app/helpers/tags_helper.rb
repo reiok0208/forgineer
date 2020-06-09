5da34dbca836e7b3a74c3c3f2608dbe929e44e8c
reiok0208 <rei.okada.wii@gmail.com>
2020-06-09 15:42:46 +0900

    [修正]タグ機能の重複処理のブラッシュアップ

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,7 @@
 module TagsHelper
+  def tag_dup_valid(value)
+    cut_full_width = value.tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z') #全角英数字を半角英数字に
+    space_delete = cut_full_width.gsub(/ |　/){""} #全ての半角全角スペースを消す
+    space_delete.downcase #重複検証のため全て小文字に変換
+  end
 end
