0daf538587223c01c41181fa94a8527957256f7c
reiok0208 <rei.okada.wii@gmail.com>
2020-06-13 13:02:12 +0900

    [修正]コードの削減など

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "テスト太郎" }
     nickname { "テスト" }
+    introduction { "よろしく" }
     sequence(:email) { |n| "test#{n}@example.com" }
     password { "testTarou1111" }
   end
