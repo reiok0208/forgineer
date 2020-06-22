1af35294da5120903b546e8e773d4a7bd7659a3b
reiok0208 <rei.okada.wii@gmail.com>
2020-06-22 11:25:08 +0900

    [修正]レビュー修正２

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,7 +1,11 @@
 module TagsHelper
+  def replace(value)
+    value.gsub(/　|ㅤ/){" "}.tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z')
+    #受け取った名前の全角英数字を半角英数字に変換、全角空白「　」(%E3%80%80)と特殊空白文字「ㅤ」（%E3%85%A4）を「 」(%20)へ変換
+  end
+
   def tag_dup_valid(value)
-    cut_full_width = value.tr('０-９ａ-ｚＡ-Ｚ','0-9a-zA-Z') #全角英数字を半角英数字に
-    space_delete = cut_full_width.gsub(/ |　/){""} #全ての半角全角スペースを消す
-    space_delete.downcase #重複検証のため全て小文字に変換
+    value.gsub!(/ /){""} #全ての半角スペースを消す
+    value.downcase! #重複検証のため全て小文字に変換
   end
 end
