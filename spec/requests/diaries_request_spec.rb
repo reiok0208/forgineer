3947dd72981d938245a089fec7f2c655c7e88059
reiok0208 <rei.okada.wii@gmail.com>
2020-06-24 14:53:37 +0900

    [追加]カバレッジ確認後テスト追加 タグ,ユーザー,日記

--- spec/requests/diaries_request_spec.rb
+++ spec/requests/diaries_request_spec.rb
@@ -41,6 +41,10 @@ RSpec.describe "Diaries", type: :request do
       it '文字列「日記編集」が正しく表示されていること' do
         expect(response.body).to include("日記編集")
       end
+      it '他のユーザーのidで日記編集へ遷移した場合トップに戻る' do
+        get edit_user_diary_path(@user2)
+        expect(response).to redirect_to root_path
+      end
     end
 
     context "日記一覧ページが正しく表示される" do
@@ -55,6 +59,18 @@ RSpec.describe "Diaries", type: :request do
       end
       it '文字列「日記一覧」が正しく表示されていること' do
         expect(response.body).to include("日記一覧")
+      end
+      it 'params[search]が含まれていた場合、文字列「フォーム検索」が正しく表示されていること' do
+        get diaries_path(search: "user")
+        expect(response.body).to include("フォーム検索")
+      end
+      it 'params[tag_id]が含まれていた場合、文字列「タグ検索」が正しく表示されていること' do
+        get diaries_path(tag_id: 1)
+        expect(response.body).to include("タグ検索")
+      end
+      it 'params[user_id]が含まれていた場合、文字列「さんの投稿日記」が正しく表示されていること' do
+        get diaries_path(user_id: @user.id)
+        expect(response.body).to include("さんの投稿日記")
       end
     end
 
@@ -72,6 +88,67 @@ RSpec.describe "Diaries", type: :request do
         expect(response.body).to include("日記詳細")
       end
     end
+  end
+
+  describe '日記投稿・日記編集・日記削除テスト' do
+    context "カレントユーザーの場合" do
+      it "正常に日記を投稿できる" do
+        expect {
+          post user_diaries_path(@user), params: { diary: {title:"日記", body:"日記", user_id: @user.id} }
+        }.to change(@user.diaries, :count).by(1)
+        expect(flash[:notice]).to include("日記を投稿しました")
+      end
+      it "不足項目がある場合新規投稿できない" do
+        expect {
+          post user_diaries_path(@user), params: { diary: {title:"", body:"日記", user_id: @user.id} }
+        }.to change(@user.diaries, :count).by(0)
+        expect(response).to render_template :new
+      end
+
+      it "正常に日記を更新できる" do
+        expect {
+          patch "/users/#{@user.id}/diaries/#{@diary.id}", params: { diary: {title:"更新", body:"更新", user_id: @user.id} }
+        }.to change(@user.diaries, :count).by(0)
+        expect(flash[:notice]).to include("日記を更新しました")
+      end
+      it "不足項目がある場合更新できない" do
+        expect {
+          patch "/users/#{@user.id}/diaries/#{@diary.id}", params: { diary: {title:"", body:"更新", user_id: @user.id} }
+        }.to change(@user.diaries, :count).by(0)
+        expect(response).to render_template :edit
+      end
+
+      it "正常に日記を削除できる" do
+        expect {
+          delete "/users/#{@user.id}/diaries/#{@diary.id}"
+        }.to change(@user.diaries, :count).by(-1)
+        expect(flash[:notice]).to include("日記を削除しました")
+      end
+
+    end
+    context "他ユーザーidの場合" do
+      it "投稿しようとするとトップ画面にリダイレクトする" do
+        expect {
+          post user_diaries_path(@user2), params: { diary: {title:"日記", body:"日記", user_id: @user.id} }
+        }.to change(@user.diaries, :count).by(0)
+        expect(response).to redirect_to root_path
+      end
+
+      it "更新しようとするとトップ画面にリダイレクトする" do
+        expect {
+          patch "/users/#{@user2.id}/diaries/#{@diary.id}", params: { diary: {title:"更新", body:"更新", user_id: @user.id} }
+        }.to change(@user.diaries, :count).by(0)
+        expect(response).to redirect_to root_path
+      end
+
+      it "削除しようとするとトップ画面にリダイレクトする" do
+        expect {
+          delete "/users/#{@user2.id}/diaries/#{@diary.id}"
+        }.to change(@user.diaries, :count).by(0)
+        expect(response).to redirect_to root_path
+      end
+    end
+    
   end
 
 end
